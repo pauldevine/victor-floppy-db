475a6234fc6431286fcea52cdca69a79b10c547b
Paul Devine <pauldevine+git@gmail.com>
2023-12-11 09:37:42 -0500

    forgot to add scripts directory

---
+++ scripts/download_iarchive.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import json
+import django
+import internetarchive as ia
+import dateparser
+
+sys.path.insert(0, '/Users/pauldevine/projects/disk_db/victordisk')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "victordisk.settings")
+django.setup()
+
+from floppies.models import Entry, ArchCollection, Contributor, Creator, Language, Subject, Mediatype
+
+def save_metadata_to_django(email):
+    search_query = f'uploader:{email} AND mediatype:software'
+    search_results = ia.search_items(search_query)
+
+    for result in search_results:
+        #identifier,file,description,subject[0],subject[1],subject[2],title,creator,date,collection,mediatype,contributor,language
+        item_id = result['identifier']
+        item = ia.get_item(item_id)
+        metadata = item.metadata
+        print("Identifier " + result['identifier'] + " " + metadata.get('title', ''))
+
+        publicationDate_dt=dateparser.parse(metadata.get('publicdate', None))
+
+        entry = Entry(
+            identifier = metadata.get('identifier', ''),
+            title = metadata.get('title', ''),
+            publicationDate = publicationDate_dt,
+            description = metadata.get('description', ''),
+            uploaded = True
+        )
+        entry.save()
+
+        # Handling ManyToMany fields like creators, languages, subjects
+        creator_names = metadata.get('creator', [])
+        if isinstance(creator_names, str):
+            creator_names = [creator_names]
+        for name in creator_names:
+            creator, created = Creator.objects.get_or_create(name=name)
+            entry.creators.add(creator)
+
+        # Handling collections
+        collection_names = metadata.get('collection', [])
+        if isinstance(collection_names, str):
+            collection_names = [collection_names]
+        for name in collection_names:
+            collection, created = ArchCollection.objects.get_or_create(name=name)
+            entry.collections.add(collection)
+
+        # Handling contributors
+        contributor_names = metadata.get('contributor', [])
+        if isinstance(contributor_names, str):
+            contributor_names = [contributor_names]
+        for name in contributor_names:
+            contributor, created = Contributor.objects.get_or_create(name=name)
+            entry.contributors.add(contributor)
+
+        # Handling languages
+        language_names = metadata.get('language', [])
+        if isinstance(language_names, str):
+            language_names = [language_names]
+        for name in language_names:
+            language, created = Language.objects.get_or_create(name=name)
+            entry.languages.add(language)
+
+        # Handling subjects
+        subject_names = metadata.get('subject', [])
+        if isinstance(subject_names, str):
+            subject_names = subject_names.split(";")
+        for name in subject_names:
+            subject, created = Subject.objects.get_or_create(name=name)
+            entry.subjects.add(subject)
+
+        # Handling Mediatype if applicable
+        mediatype_str = metadata.get('mediatype', [])
+        mediatype_key = Mediatype.get_mediatype_key(mediatype_str)
+        if mediatype_key:
+            mediatype, created = Mediatype.objects.get_or_create(mediatype=mediatype_key)
+            entry.mediatype = mediatype
+            entry.save()
+
+print("downloading Internet Arhive data for paul.devine@gmail.com")
+save_metadata_to_django('paul.devine@gmail.com')
---
+++ scripts/missing_folder.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import json
+import django
+import internetarchive as ia
+import dateparser
+from django.core.files import File
+
+sys.path.insert(0, '/Users/pauldevine/projects/disk_db/victordisk')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "victordisk.settings")
+django.setup()
+mustering_dir = "/Users/pauldevine/Documents/Victor9k Stuff/Disk Mustering Area/"
+
+from floppies.models import Entry, ArchCollection, Contributor, Creator, Language, Subject, Mediatype
+
+def check_file_exists(directory, filename):
+    if not isinstance(directory, str) or not isinstance(filename, str):
+        raise ValueError("Both directory and filename must be strings")
+
+    # Construct the full file path
+    file_path = os.path.join(directory, filename)
+
+    # Check if the file exists at the specified path
+    return os.path.isdir(file_path)
+
+def get_entries():
+  entries = Entry.objects.all()
+  for item in entries:
+    exists = check_file_exists(mustering_dir, item.title)
+    if not exists:
+        print(item.title, exists)
+    
+  
+
+print("Linking data to file system")
+get_entries()
